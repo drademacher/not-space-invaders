198c8b2e05023bec629ed47575f70ae6496a4375
drademacher <danny.rademacher@gmail.com>
2023-11-19 00:15:54 +0100

    Initial pixi.js + webpack setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Application, Sprite } from 'pixi.js'
+
+const app = new Application({
+	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+	resolution: window.devicePixelRatio || 1,
+	autoDensity: true,
+	backgroundColor: 0x6495ed,
+	width: 640,
+	height: 480
+});
+
+const logo: Sprite = Sprite.from("testing.png");
+
+logo.anchor.set(0.5);
+
+logo.x = app.screen.width / 2;
+logo.y = app.screen.height / 2;
+
+app.stage.addChild(logo);
